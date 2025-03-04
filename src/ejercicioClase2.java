215bc1090d72926e0bff545dc12ae6b0865463e0
kevin <kevinlibardorosas@gmail.com>
2025-03-04 07:52:22 -0500

    Se crea ejercicio dos

---
+++ src/ejercicioClase2.java
@@ -0,0 +1,14 @@
+public class ejercicioClase2 {
+    public static void main(String[] args) {
+        int contador = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i%2==0) {
+                contador++;
+                
+            }
+
+            
+        }
+        System.out.println("Hay un total de : " + contador + " numeros pares");
+    }
+}
