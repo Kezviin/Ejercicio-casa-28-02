b4dedda9afe59d42a61896f52b868eb090c09345
kevin <kevinlibardorosas@gmail.com>
2025-03-04 07:53:10 -0500

    Se crea ejercicio 4

---
+++ src/ejercicioClase4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ejercicioClase4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa el numero para sacar factorial");
+        int numUno = scanner.nextInt();
+        int factorial = 1;
+        int i = 1;
+        while (i<=numUno) {
+            factorial*=i;
+            i++;
+            
+        }
+        System.out.println("El factorial de: " + numUno + " Es : " + factorial);
+    }
+}
