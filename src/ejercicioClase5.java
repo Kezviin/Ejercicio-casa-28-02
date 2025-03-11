1da2435b4f7e5010741f7191089dcefcc73c8ce0
kezvin <kevinlibardorosas@gmail.com>
2025-03-11 06:27:48 -0500

    Se agrega el ejercicio 5

--- src/ejercicioClase5.java
+++ src/ejercicioClase5.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class ejercicioClase5 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un número: ");
+        int numero = scanner.nextInt();
+        System.out.println("LA TABLA DE MULTIPLICAR DEL NUMERO " + numero + " Es:");
+
+        for (int i = 1; i <= 10; i++) {
+            
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+    }
 }
