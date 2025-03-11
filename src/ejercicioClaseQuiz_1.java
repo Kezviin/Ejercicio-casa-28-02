6e16738b4480e5007385e014dc1386c7185bc24e
kezvin <kevinlibardorosas@gmail.com>
2025-03-11 07:42:50 -0500

    Se crea ejercicio quiz 1

---
+++ src/ejercicioClaseQuiz_1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ejercicioClaseQuiz_1 {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un numero");
+        int num = scanner.nextInt();
+        if (num<=0) {
+            System.out.println("El numero ingresado no es valido");
+            scanner.close();
+            return;
+        }
+        for (int i = num; i >= 1; i--) {
+            for (int espacio = 1; espacio < i; espacio++) {
+                System.out.print("  ");
+            }
+
+            for (int j = i; j <= num; j++) {
+                System.out.print(j + " ");
+            }
+
+            for (int j = num - 1; j >= i; j--) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+        scanner.close();
+        }
+        
+        
+    }
+
