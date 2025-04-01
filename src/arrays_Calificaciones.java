c02d1f4b5b5b858ee07c1d4cd00951c31ba311df
kezvin <kevinlibardorosas@gmail.com>
2025-04-01 07:07:01 -0500

    se agrega arquitectura java de  ejercicios

---
+++ src/arrays_Calificaciones.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class arrays_Calificaciones {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = {5,3,8,1,9};
+        System.out.println(numeros[]);
+        
+        
+    }
+}
